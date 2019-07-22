3b3b0ac4737d07c0b79f2b3ee6fb4bbaf359e544
Matt <matthew.mckee@live.com>
2019-07-22 17:51:28 -0400

    init commit

---
+++ types.ts
@@ -0,0 +1,34 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+myString = "Hello world";
+myNum = 22;
+myBool = true;
+myVar = "hello";
+
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[];
+
+strArr = ["hello", "world"];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+
+let strArrAlt: Array<string>;
+let numArrAlt: Array<number>;
+let boolArrAlt: Array<boolean>;
+
+strArrAlt = ["hello", "world"];
+numArrAlt = [1, 2, 3];
+boolArrAlt = [true, false, true];
+
+let strNumTuple: [string, number];
+let myVoid: void = null;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+
+strNumTuple = ["Hello", 4];
+
+console.log(myUndefined);
